80cbe217f45d87e435aa75816c026285caa85c25
Pardiniii <ggpardini@gmail.com>
2024-12-13 15:53:08 -0300

    alterando o tema do app

--- app/src/main/java/com/gustavo/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/gustavo/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -26,6 +26,7 @@ class FormularioProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        title = "Cadastrar produto"
         configuraBotaoSalvar()
 
         binding.fomrularioProdutoImagem.setOnClickListener {
